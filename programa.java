22e093bed4e25f843d090943f257fedb056266ff
rubicmaster <mrrubenjimeneznu@gmail.com>
2026-02-24 04:01:56 +0100

    Update programa.java

--- programa.java
+++ programa.java
@@ -1,5 +1,5 @@
 public static void main(String[] args) {
     System.out.println("esta es una prueba");
 
-    System.out.println("esto es una prueba del colaborador");
+    System.out.println("esto es una prueba del cadolaborador");
 }
